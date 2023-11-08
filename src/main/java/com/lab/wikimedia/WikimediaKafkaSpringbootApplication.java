fcf3c4647f3460345b0952c9a6e4db1c8572f7ca
hadipsy27 <hadipsy27@gmail.com>
2023-11-08 16:20:56 +0700

    First commit

---
+++ src/main/java/com/lab/wikimedia/WikimediaKafkaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lab.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaKafkaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaKafkaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/wikimedia/WikimediaKafkaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.wikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaKafkaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

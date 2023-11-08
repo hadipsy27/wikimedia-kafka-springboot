96bbc5517524d73662d554d6d3daeeb462a9c90a
hadipsy27 <hadipsy27@gmail.com>
2023-11-08 16:41:38 +0700

    add springboot run application in module kafka-producer-wikimedia

--- kafka-producer-wikimedia/src/main/java/com/lab/SpringbootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/lab/SpringbootProducerApplication.java
@@ -1,7 +1,10 @@
 package com.lab;
 
+import org.springframework.boot.SpringApplication;
+
 public class SpringbootProducerApplication {
     public static void main(String[] args) {
+        SpringApplication.run(SpringbootProducerApplication.class);
         System.out.println("Hello world!");
     }
 }

e8eb9d8c1e1ff20372543a1919225ef4aae9e627
hadipsy27 <hadipsy27@gmail.com>
2023-11-10 14:56:50 +0700

    222. Kafka Consumer Implementation

--- kafka-consumer-database/src/main/java/com/lab/SpringbootConsumerApplication.java
+++ kafka-consumer-database/src/main/java/com/lab/SpringbootConsumerApplication.java
@@ -1,10 +1,11 @@
 package com.lab;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootConsumerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(SpringbootConsumerApplication.class);
     }
 }

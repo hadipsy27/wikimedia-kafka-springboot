59d8a6b87b9d3ce59793caffd3d41fe03d33cf98
hadipsy27 <hadipsy27@gmail.com>
2023-11-10 14:08:28 +0700

    220. Kafka Consumer Project Setup

---
+++ kafka-consumer-database/src/main/java/com/lab/SpringbootConsumerApplication.java
@@ -0,0 +1,10 @@
+package com.lab;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConsumerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

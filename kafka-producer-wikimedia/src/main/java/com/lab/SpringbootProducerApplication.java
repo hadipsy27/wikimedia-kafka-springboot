4796dbfacbc770f46cdab06c3a53ad986772e550
hadipsy27 <hadipsy27@gmail.com>
2023-11-10 13:59:07 +0700

    219. Run Wikimedia Producer

--- kafka-producer-wikimedia/src/main/java/com/lab/SpringbootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/lab/SpringbootProducerApplication.java
@@ -1,10 +1,22 @@
 package com.lab;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class SpringbootProducerApplication {
+@SpringBootApplication
+public class SpringbootProducerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootProducerApplication.class);
         System.out.println("Hello world!");
+    }
+
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
